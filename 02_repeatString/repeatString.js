7e5a4f601750ffe2195e8032a66b691a2deacbb6
Mike Neal <mike@bluepixel.dev>
2023-10-30 23:28:06 -0400

    finish 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+	let returnString = '';
+	if (num < 0) {
+		return 'ERROR'
+	} else {
+		for (let i = 0; i < num; i++) {
+				returnString = returnString.concat(string)
+		}
+	}
+	return returnString;
 
 };
 
