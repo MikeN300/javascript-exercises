5d2d7f738ba885b5ca25514e3d80ba860d132788
Mike Neal <mike@bluepixel.dev>
2023-10-31 01:01:10 -0400

    finish 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	let splitString = string.split("");
+	let splitStringReverse = [];
+	let x = 0;
+	for (let i = splitString.length; i >= 0; i--) {
+		splitStringReverse[x] = splitString[i];
+		x++;
+	}
+	return splitStringReverse.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
+// const removeFromArray = function(array, ...args) {
+// 	let returnArray = []
+// 	for (let i = 0; i <= args.length; i++) {
+// 		for (let x = 0; x <= array.length; x++) {
+// 			if (array[x] !== args[i]) {
+// 				returnArray.push(array[i])
+// 			}
+// 		}
+//  	}
+// 	return returnArray;
+// };
 
+
+const removeFromArray = function(array, ...args) {
+	for (let i = 0; i <= args.length; i++) {
+		for (let x = array.length; x >= 0; x--) {
+			if(array[x] === args[i]) {
+				array.splice(x, 1)
+			}
+		}
+	}
+	return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
