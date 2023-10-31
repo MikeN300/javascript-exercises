ee1f93e2419883c11c5674f7efa82c7f4cbd4509
Mike Neal <mike@bluepixel.dev>
2023-10-31 01:27:22 -0400

    05 and 06 finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,3 @@
-// const removeFromArray = function(array, ...args) {
-// 	let returnArray = []
-// 	for (let i = 0; i <= args.length; i++) {
-// 		for (let x = 0; x <= array.length; x++) {
-// 			if (array[x] !== args[i]) {
-// 				returnArray.push(array[i])
-// 			}
-// 		}
-//  	}
-// 	return returnArray;
-// };
-
-
 const removeFromArray = function(array, ...args) {
 	for (let i = 0; i <= args.length; i++) {
 		for (let x = array.length; x >= 0; x--) {
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+	//create a variable that holds the final sum
+	if (num1 < 0 || !Number.isInteger(num1) || num2 < 0 || !Number.isInteger(num2) ) return "ERROR";
+	let finalSum = 0;
+	//create 2 consts, use them to determine the higher of the two args
+	let higherArg = 0;
+	let lowerArg = 0;
+	if (num1 > num2) {
+		higherArg = num1;
+		lowerArg = num2;
+	} else {
+		higherArg = num2;
+		lowerArg = num1;
+	}
+	//create a loop that starts with the lower arg and loops to the higher arg
+	for(let i = lowerArg; i <= higherArg; i++) {
+		finalSum += i;
+	}
+	//add i to the sum variable
+	//return the sum
+	return finalSum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
